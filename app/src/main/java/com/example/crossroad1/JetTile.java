c7a79b3c57556507ec40234536712b8ae0dd08b7
Nandha Thiruvengadam <nandhakt@lawn-128-61-65-205.lawn.gatech.edu>
2023-03-14 01:11:11 -0400

    Add JetTile and UFOTile functionality

--- app/src/main/java/com/example/crossroad1/JetTile.java
+++ app/src/main/java/com/example/crossroad1/JetTile.java
@@ -5,5 +5,30 @@ public class JetTile extends Tile {
     private int jetImage;
     private boolean hasJetSprite;
 
+    public JetTile(Coordinate loc) {
+        super();
+        this.jetCoord = loc;
+        this.jetImage = R.drawable.jet;
+        super.setTileType(jetImage);
+    }
 
+    @Override
+    public void setImage() {
+        if (hasJetSprite) {
+            this.jetImage = R.drawable.jet;
+        }
+    }
+
+    @Override
+    public int getImage() {
+        return jetImage;
+    }
+
+    public void addSprite() {
+        this.jetImage = R.drawable.jet;
+    }
+
+    public void removeSprite() {
+        this.jetImage = R.drawable.road_tile;
+    }
 }
--- app/src/main/java/com/example/crossroad1/UFOTile.java
+++ app/src/main/java/com/example/crossroad1/UFOTile.java
@@ -1,7 +1,34 @@
 package com.example.crossroad1;
 
 public class UFOTile extends Tile {
+    private Coordinate ufoCoord;
+    private int ufoImage;
+    private boolean hasUfoSprite;
+
     public UFOTile(Coordinate loc) {
-        super(loc, R.drawable.ufo);
+        super();
+        this.ufoCoord = loc;
+        this.ufoImage = R.drawable.jet;
+        super.setTileType(ufoImage);
+    }
+
+    @Override
+    public void setImage() {
+        if (hasUfoSprite) {
+            this.ufoImage = R.drawable.ufo;
+        }
+    }
+
+    @Override
+    public int getImage() {
+        return ufoImage;
+    }
+
+    public void addSprite() {
+        this.ufoImage = R.drawable.ufo;
+    }
+
+    public void removeSprite() {
+        this.ufoImage = R.drawable.road_tile;
     }
 }
